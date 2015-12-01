1a211b7bd6f9276881a63012670cdf04e4662a55
resteve <resteve@zikzakmedia.com>
2015-12-01 11:01:43 +0100

    Tests

--- tests/test_account_payment_sepa_es.py
+++ tests/test_account_payment_sepa_es.py
@@ -1,27 +1,18 @@
-#!/usr/bin/env python
+# This file is part of the account_payment_sepa_es module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_sepa_es')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_sepa_es')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountPaymentSepaEsTestCase(ModuleTestCase):
+    'Test Account Payment Sepa Es module'
+    module = 'account_payment_sepa_es'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        AccountPaymentSepaEsTestCase))
     return suite
