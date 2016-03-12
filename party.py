acd8d8e58ac827002534a1efb08cb19995e1bfd3
Albert Cervera i Areny <albert@nan-tic.com>
2016-03-12 23:23:26 +0100

    Do not override get_sepa_creditor_identifier_used() to raise any exceptions. (grafted from ad23150114273fe6ee38beac26aab0cf5d3da8a2)

--- party.py
+++ party.py
@@ -21,10 +21,7 @@ class Party:
         super(Party, cls).__setup__()
         cls._error_messages.update({
                 'invalid_creditor_identifier': ('Invalid creditor identifier '
-                    '"%s" for party "%s".'),
-                'missing_creditor_identifier': ('Missing creditor identifier '
-                    'for party "%s".'),
-                'missing_vat_number': ('Missing VAT number for party "%s".'),
+                    '"%(identifier)s" for party "%(party)s".'),
                 })
         cls._buttons.update({
                 'calculate_sepa_creditor_identifier': {
@@ -42,8 +39,10 @@ class Party:
         if not self.sepa_creditor_identifier:
             return
         if not is_valid(self.sepa_creditor_identifier):
-            self.raise_user_error('invalid_creditor_identifier',
-                (self.sepa_creditor_identifier, self.rec_name))
+            self.raise_user_error('invalid_creditor_identifier', {
+                    'identifier': self.sepa_creditor_identifier,
+                    'party': self.rec_name,
+                    })
 
     @classmethod
     @ModelView.button
@@ -57,21 +56,3 @@ class Party:
             party.sepa_creditor_identifier = (party.vat_country + check_sum +
                 'ZZZ' + party.vat_number.upper())
             party.save()
-
-    def get_sepa_creditor_identifier_used(self, name):
-        res = super(Party, self).get_sepa_creditor_identifier_used(name)
-        suffix = Transaction().context.get('suffix', None)
-        kind = Transaction().context.get('kind', '')
-        if kind == 'payable':
-            if not self.vat_number:
-                self.raise_user_error('missing_vat_number', (self.rec_name,))
-            res = self.vat_number
-        if suffix:
-            if not res:
-                self.raise_user_error('missing_creditor_identifier',
-                    (self.rec_name,))
-            if kind == 'receivable':
-                res = res[:4] + suffix + res[7:]
-            else:
-                res = res + suffix
-        return res
