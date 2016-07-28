e3546476d9f2b25a934f40fd2b49d8c4bb4db843
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-07-28 17:02:53 +0200

    Remove payment_type and bank account from Account Move Line

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .account import *
 from .payment import *
 from .party import *
 
@@ -13,8 +12,6 @@ def register():
         Payment,
         Party,
         Mandate,
-        BankNumber,
-        MoveLine,
         module='account_payment_sepa_es', type_='model')
     Pool.register(
         PayLine,
