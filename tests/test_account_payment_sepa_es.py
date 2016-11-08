233824c2d358c7cd374e59705b3ed723e5cda8e2
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-11-08 15:27:19 +0100

    Add test for SEPA Creditor Identifier calculation

--- tests/test_account_payment_sepa_es.py
+++ tests/test_account_payment_sepa_es.py
@@ -3,12 +3,30 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 
 
 class AccountPaymentSepaEsTestCase(ModuleTestCase):
     'Test Account Payment Sepa Es module'
     module = 'account_payment_sepa_es'
+
+    @with_transaction()
+    def test_sepa_identifier(self):
+        'Test sepa indentifier validation'
+        pool = Pool()
+        Party = pool.get('party.party')
+        Identifier = pool.get('party.identifier')
+
+        party = Party(name='test')
+        party.save()
+        Party.calculate_sepa_creditor_identifier([party])
+        self.assertIsNone(party.sepa_creditor_identifier_used)
+        sepa = Identifier(party=party, code='ES47690558N', type='eu_vat')
+        sepa.save()
+        Party.calculate_sepa_creditor_identifier([party])
+        self.assertEqual(party.sepa_creditor_identifier_used,
+            'ES23ZZZ47690558N')
 
 
 def suite():
