60e157b3fd4fccbe537ea7eeb014e24e500706ba
Bernat Brunet Torruella <bernat@nan-tic.com>
2017-01-12 14:16:20 +0100

    Add mandate report

--- __init__.py
+++ __init__.py
@@ -1,21 +1,22 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .account import *
-from .payment import *
-from .party import *
+from . import accout, payment, party
 
 
 def register():
     Pool.register(
-        Journal,
-        Group,
-        Payment,
-        Party,
-        Mandate,
-        BankNumber,
-        MoveLine,
+        payment.Journal,
+        payment.Group,
+        payment.Payment,
+        party.Party,
+        payment.Mandate,
+        account.BankNumber,
+        account.MoveLine,
         module='account_payment_sepa_es', type_='model')
     Pool.register(
-        PayLine,
+        payment.PayLine,
         module='account_payment', type_='wizard')
+    Pool.register(
+        payment.MandateReport,
+        module='account_payment_sepa_es', type_='report')
