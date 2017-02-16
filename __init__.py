71774a5cedb78e0b597f5cc00a0bd656695f0586
Raimon Esteve <raimon@nan-tic.com>
2017-02-16 15:50:06 +0100

    Import __init__ with diferents imports

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import account, payment, party
+from . import account
+from . import payment
+from . import party
 
 
 def register():
