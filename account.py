ee52614301eccc67acca6201c6c173789d236dbd
Raimon Esteve <raimonesteve@gmail.com>
2022-02-25 13:30:25 +0100

    Filter sepa_mandate by company

--- account.py
+++ account.py
@@ -23,6 +23,8 @@ class PayLine(metaclass=PoolMeta):
             mandate = None
             for number in numbers:
                 for sepa_mandate in number.sepa_mandates:
+                    if sepa_mandate.company != payment.company:
+                        continue
                     if sepa_mandate.party == payment.party:
                         mandate = sepa_mandate
                         break
